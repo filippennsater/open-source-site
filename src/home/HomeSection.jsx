339efaba20a761de6710e1837a27c1def84f7a34
filippennsater <filip.pennsater@hotmail.com>
2024-06-11 21:50:03 +0200

    installed sass and react router... also styled HomeSection

---
+++ src/home/HomeSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+import './HomeSection.scss';
+
+
+function HomeSection() {
+
+
+    return (
+    <div className='full-screen-container'>
+         <div className='OP-header'>
+            ewgmknoerg
+         </div>
+         <div className='OP-text'>
+            iotpetpoi
+         </div>
+    </div>
+    )
+}
+
+export default HomeSection
